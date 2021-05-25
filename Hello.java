113221c5516f879ac74c01c9705679aa100c32ca
kwonh9898 <69881537+kwonh9898@users.noreply.github.com>
2021-05-25 00:20:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*****************************************************
+* Java Program Name
+******************************************************
+* Hanna Kwon
+* 5/24/21
+* CMSC 255 - C01
+*****************************************************/
+
+public class Hello
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hanna Kwon");
+		System.out.println("Java Program Name");
+		System.out.println("Printing student name, date and course number");
+		System.out.println("CMSC 255 - C01");
+	}
+}
